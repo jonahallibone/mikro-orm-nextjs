820669924228a828e7ce10f5a20a2d230167abb6
Jonas Strassel <info@jonas-strassel.de>
2021-11-22 10:43:26 +0000

    chore: upgrade to nextjs 11

--- config/mikro-orm.ts
+++ config/mikro-orm.ts
@@ -2,12 +2,8 @@ import { User } from "../entities/User";
 import { Options } from "@mikro-orm/core";
 
 const config: Options = {
-  dbName: "async-up",
-  type: "mysql",
-  host: process.env.MYSQL_HOST,
-  port: Number(process.env.MYSQL_PORT),
-  user: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
+  dbName: "test.db",
+  type: "sqlite",
   entities: [User],
   debug: process.env.NODE_ENV === "development",
 };
